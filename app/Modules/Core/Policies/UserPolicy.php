8ff8c3546b00658fe248ba64eb8277f864c5d753
Chen Sakkol <chensakkol2024@gmail.com>
2026-03-31 18:53:56 +0700

    fix route Usermanagement

--- app/Modules/Core/Policies/UserPolicy.php
+++ app/Modules/Core/Policies/UserPolicy.php
@@ -8,33 +8,30 @@ use App\Modules\Core\Models\User;
 
 class UserPolicy
 {
-    protected function isAdmin(User $user): bool
-    {
-        return $user->hasRole('admin', 'api');
-    }
+
 
     public function viewAny(User $user): bool
     {
-        return $this->isAdmin($user) || $user->hasPermissionTo('users.view', 'api');
+        return $user->hasPermissionTo('users.view', 'api');
     }
 
     public function view(User $user, User $model): bool
     {
-        return $this->isAdmin($user) || $user->hasPermissionTo('users.view', 'api');
+        return $user->hasPermissionTo('users.view', 'api');
     }
 
     public function create(User $user): bool
     {
-        return $this->isAdmin($user) || $user->hasPermissionTo('users.create', 'api');
+        return $user->hasPermissionTo('users.create', 'api');
     }
 
     public function update(User $user, User $model): bool
     {
-        return $this->isAdmin($user) || $user->hasPermissionTo('users.edit', 'api');
+        return $user->hasPermissionTo('users.edit', 'api');
     }
 
     public function delete(User $user, User $model): bool
     {
-        return $this->isAdmin($user) || $user->hasPermissionTo('users.delete', 'api');
+        return $user->hasPermissionTo('users.delete', 'api');
     }
 }
